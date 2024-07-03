2d79502fa7b67df3b8a860fc83edf8a36e54d46d
Ben Walker <ben.walker18@gmail.com>
2024-07-02 17:15:26 -0700

    Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,29 @@
-module.exports = {
-  files: ["**/*.ts", "**/*.tsx"],
-};
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json', // Ensures the parser uses your tsconfig
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  ...compat.extends('prettier'),
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+];
